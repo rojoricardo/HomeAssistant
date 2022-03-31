04e767f017c07f937123595bfeae05d5f840d340
rojoricardo <89477800+rojoricardo@users.noreply.github.com>
2022-03-30 22:48:45 -0500

    d

---
+++ UCTronics_OLED_Display_Python/python/fan.py
@@ -0,0 +1,18 @@
+import smbus
+import time
+bus = smbus.SMBus(1)
+
+addr = 0x0d
+fan_reg = 0x08
+state = 0
+
+while True:
+    if state == 0:
+        bus.write_byte_data(addr, fan_reg, 0x00)
+        time.sleep(2)
+    elif state == 1:
+        bus.write_byte_data(addr, fan_reg, 0x01)
+        time.sleep(2)
+
+    state = (state + 1) % 2
+    
